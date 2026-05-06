5966fd49500fd854e2f5c2cfc3d0eb1718d79a44
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2026-05-06 23:16:11 +0500

    Images

--- app/blog/3/page.tsx
+++ app/blog/3/page.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import BlogLayout from "../layout";
+import Image from "next/image";
 
 const BlogPost = () => {
-  return <BlogLayout title="Blog Post 3">Blog Post 3 Content</BlogLayout>;
+  return (
+    <BlogLayout title="Blog Post 3">
+      <h1>Blog Post 3 Content</h1>
+      <p>This is the content for Blog Post 3.</p>
+      <Image
+        src="/public/C9.jpg"
+        alt="Blog Post 3 Image"
+        width={600}
+        height={400}
+      />
+    </BlogLayout>
+  );
 };
 
 export default BlogPost;
