c072072819b6ff57a27d4e5c5d8d9b1d492856be
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2026-05-06 23:25:13 +0500

    Builidng Image

--- app/blog/3/page.tsx
+++ app/blog/3/page.tsx
@@ -1,18 +1,14 @@
 import React from "react";
 import BlogLayout from "../layout";
 import Image from "next/image";
+import C9 from "..//../../public/C9.jpg";
 
 const BlogPost = () => {
   return (
     <BlogLayout title="Blog Post 3">
       <h1>Blog Post 3 Content</h1>
       <p>This is the content for Blog Post 3.</p>
-      <Image
-        src="/public/C9.jpg"
-        alt="Blog Post 3 Image"
-        width={600}
-        height={400}
-      />
+      <Image src={C9} alt="Blog Post 3 Image" width={600} height={400} />
     </BlogLayout>
   );
 };
