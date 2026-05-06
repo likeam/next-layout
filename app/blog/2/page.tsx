448cddf6cb338d41ad46afefe67f4d0222957019
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2026-05-07 00:27:36 +0500

    Images section

--- app/blog/2/page.tsx
+++ app/blog/2/page.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import BlogLayout from "../layout";
+import Image from "next/image";
 
 const BlogPost = () => {
-  return <BlogLayout title="Blog Post 2">Blog Post 2 Content</BlogLayout>;
+  return (
+    <BlogLayout title="Blog Post 2">
+      <h1>Blog Post 2 Content</h1>
+      <p>This is the content for Blog Post 2.</p>
+      <Image
+        src="https://images.pexels.com/photos/36703945/pexels-photo-36703945.jpeg"
+        alt="Blog Post 2 Image"
+        width={600}
+        height={400}
+      />
+    </BlogLayout>
+  );
 };
 
 export default BlogPost;
